10cd3a9470b2625a2712bdcc312abfbb2c500cb0
Vikrant Malla <vikrantmalla999@gmail.com>
2024-02-15 21:51:00 +0545

    feat: add a faq question and answer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,49 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h2>Frequently Asked Questions</h2>
+    <div v-for="(faq, index) in faqs" :key="index">
+      <h3 @click="toggleAnswer(index)">{{ faq.question }}</h3>
+      <p v-if="activeIndex === index">{{ faq.answer }}</p>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<script setup lang="ts">
+import { Ref, ref } from "vue";
+const faqs = ref([
+  {
+    question: "What is Frontend Mentor, and how will it help me?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+  },
+  {
+    question: "Is Frontend Mentor free?",
+    answer:
+      "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+  },
+  {
+    question: "Can I use Frontend Mentor projects in my portfolio?",
+    answer:
+      "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+  },
+  {
+    question: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+    answer:
+      "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+  },
+]);
+
+const activeIndex: Ref<number | null> = ref(null);
+
+const toggleAnswer = (index: number) => {
+  if (activeIndex.value === index) {
+    activeIndex.value = null;
+  } else {
+    activeIndex.value = index;
+  }
+};
+
+</script>
 
 <style scoped>
 .logo {
